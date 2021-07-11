0ff8e0c792d0380f6668a09bba8bec3b4619f31a
Dheeraj singh <47826524+Dheerajsingh0@users.noreply.github.com>
2021-07-11 12:03:35 +0530

    Update Thumbnail.js

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,7 +6,7 @@ const Thumbnail=forwardRef(({result},ref) => {
     const BASE_URL="https://image.tmdb.org/t/p/original/"
     return (
         <div ref={ref} className="group cursor-pointer transition w-full  md:p-2 md:m-2 ">
-            <Image className="duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+            <Image className="duration-300 ease-in transform hover:scale-105 hover:z-50"
             layout="responsive"
             src={`${BASE_URL}${result.backdrop_path || result.poster_path}`||
             `${BASE_URL}${result.poster_path}`
